e7c2ef937a821a8676fbbaef01a479066d72e1b3
abhijeet.majgaonkar <abhijeet.majgaonkar@M-2005.local>
2015-10-23 12:25:56 +0530

    Application code initiated

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); 
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+var server = http.createServer(app).listen(app.get('port') ,app.get('ip'),function(){
+	console.log('App is running at '+ app.get('ipaddr') +':'+ app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname+'/public'));
+
+io.on('connection', function (socket) {
+	  socket.emit('init:connection', { data: 'socket connected' });
+	  
+	  socket.on('draw:line', function (data) {
+		    socket.broadcast.emit('draw:line:from:server',data)
+	  });
+	  socket.on('save:name',function(data){
+		  socket.broadcast.emit('save:name:from:server',data);
+	  });
+});
